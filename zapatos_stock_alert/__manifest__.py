9c639c421540f42918b98501cfc0e7f22fa53eae
Karen Carrera <karen.inapanta432@gmail.com>
2026-06-15 18:58:47 -0500

    Funcionamiento: modulo zapatos_stock_alert

---
+++ zapatos_stock_alert/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Alerta de stock mínimo',
+    'version': '1.0',
+    'summary': 'Zapatos con stock crítico ',
+    'description':'stock crítico',
+    'author': 'Karen Inapanta',
+    'category': 'Sales',
+    'license': 'LGPL-3',
+    'depends': ['zapatos'],
+    'data': [
+        'views/zapato_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
